c449842a4391c7859f5482d8398b2fe5e237cbd4
Alex Slatinin <sandroisu1411@gmail.com>
2025-03-02 08:45:24 +0300

    connected buttons to functions

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -9,11 +9,37 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var resultLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func numberClicked(_ sender: UIButton) {
+        if (sender.tag > 9 || sender.tag < 0) {
+            return
+        }
+        resultLabel.text = String(sender.tag)
+    }
+    
+    @IBAction func operatorClicked(_ sender: UIButton) {
+        switch sender.tag {
+            case 10:
+            resultLabel.text = "10"
+            case 11:
+            resultLabel.text = "11"
+            case 12:
+            resultLabel.text = "12"
+            case 13:
+            resultLabel.text = "13"
+            default:
+            break
+        }
+    }
+    
+    @IBAction func equalsClicked(_ sender: UIButton) {
+        
+    }
 
 }
 
