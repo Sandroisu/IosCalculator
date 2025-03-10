12f0ad5a1cb8d324891b8a8a069655fa7b224b44
Alex Slatinin <sandroisu1411@gmail.com>
2025-03-10 21:54:14 +0300

    added calculation logic

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -11,6 +11,8 @@ class ViewController: UIViewController {
     
     var currentOperator: Operator = Operator.nothing
     var calcState: CalculationState = CalculationState.enteringNumber
+    
+    var firstValue: String = ""
 
     @IBOutlet weak var resultLabel: UILabel!
     override func viewDidLoad() {
@@ -22,26 +24,67 @@ class ViewController: UIViewController {
         if (sender.tag > 9 || sender.tag < 0) {
             return
         }
-        resultLabel.text = String(sender.tag)
+        updateDisplay(number: String(sender.tag))
     }
     
     @IBAction func operatorClicked(_ sender: UIButton) {
+        calcState = CalculationState.newNumStarted
+        
+        if let num = resultLabel.text {
+            if !num.isEmpty {
+                firstValue = num
+                resultLabel.text = ""
+            }
+        }
+        
         switch sender.tag {
             case 10:
-            resultLabel.text = "10"
+            currentOperator = Operator.add
             case 11:
-            resultLabel.text = "11"
+            currentOperator = Operator.subtract
             case 12:
-            resultLabel.text = "12"
+            currentOperator = Operator.multiply
             case 13:
-            resultLabel.text = "13"
+            currentOperator = Operator.divide
             default:
             break
         }
     }
     
     @IBAction func equalsClicked(_ sender: UIButton) {
-        
+        calculate()
+    }
+                      
+    private func updateDisplay(number: String) {
+        if (calcState == CalculationState.newNumStarted) {
+            if let num = resultLabel.text {
+                if !num.isEmpty {
+                    firstValue = num
+                }
+            }
+            calcState = CalculationState.enteringNumber
+                resultLabel.text = number
+        } else if (calcState == CalculationState.enteringNumber) {
+            resultLabel.text = resultLabel.text! + number
+        }
+    }
+    
+    private func calculate() {
+        var result: String = ""
+        switch currentOperator {
+        case Operator.add:
+            result = String(Int(firstValue)! + Int(resultLabel.text!)!)
+        case Operator.subtract:
+            result = String(Int(firstValue)! - Int(resultLabel.text!)!)
+        case Operator.multiply:
+            result = String(Int(firstValue)! * Int(resultLabel.text!)!)
+        case Operator.divide:
+            result = String(Int(firstValue)! / Int(resultLabel.text!)!)
+        default:
+            break
+        }
+        resultLabel.text = result
+        calcState = CalculationState.newNumStarted
     }
 
 }
